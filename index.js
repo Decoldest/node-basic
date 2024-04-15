0b46c632c1df81118d2d1ef9e29e2cd660bdea45
Decoldest <127795154+Decoldest@users.noreply.github.com>
2024-04-15 12:51:42 -0400

    serve about.html from '/about'

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import { createServer } from "node:http";
-import * as fs from "node:fs";
+import fs from 'fs';
 
 const server = createServer((req, res) => {
   if (req.url === "/") {
     fs.readFile("index.html", (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("about.html", (err, data) => {
       if (err) throw err;
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end(data);
